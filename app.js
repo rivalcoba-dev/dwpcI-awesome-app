581b4e6b6c609c5b7d4247433f3dde72a9a6021e
rivalcoba <ivan.rivalcoba@gmail.com>
2023-05-03 18:17:17 +0000

    ⭐ Routes and middlewares

--- app.js
+++ app.js
@@ -7,15 +7,21 @@ import express from 'express';
 // que basicamente es un middleware
 const app = express();
 
-// Registrando el primer middleware
-app.use((req, res, next)=>{
-  console.log("📢 Middleware #1");
-  // Se invoca al siguiente middlware
-  next();
+// Ruta about
+// GET /about
+app.use('/about',(req, res)=>{
+  console.log("📢 Sirviendo la ruta '/about'");
+  // Se contesta al server
+  res.send(`
+    <h1>🪄 About...</h1>
+    <p>App for Fullstack Web Dev Course I!</p>
+  `);
 });
-// Registrando el segundo middleware
-app.use((req, res, next)=>{
-  console.log("📢 Middleware #2");
+
+// Ruta Raíz
+// GET /
+app.use((req, res)=>{
+  console.log("📢 Sirviendo la ruta '/'");
   // Se contesta al server
   res.send(`
     <h1>Welcome to Express Js</h1>
