22267dc6bd028292ceb1ca9d20604a41f39635b5
rivalcoba <ivan.rivalcoba@gmail.com>
2023-05-06 19:44:59 +0000

    🌟 Vistas Estaticas

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,5 +1,7 @@
 // Importando el enrutador de express
 import { Router } from 'express';
+// Importando el gestor de rutas
+import path from 'path';
 
 // Creando una instancia del enrutador de express
 const router = Router();
@@ -8,12 +10,7 @@ const router = Router();
 router.get('/add-product', (req, res, next) => {
   // Servimos el formulario
   console.log("📢 Sirviendo formulario...");
-  res.send(`
-  <form action="/add-product" method="POST">
-    <input type="text" name="title">
-    <button type="submit">Add product</button>
-  </form>
-  `);
+  res.sendFile(path.resolve('views','add-product.html'));
 });
 
 // POST /add-product
