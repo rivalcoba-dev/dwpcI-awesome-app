4d489ccc16148be93f6568eee5e4f6a21b0e84fb
rivalcoba <ivan.rivalcoba@gmail.com>
2023-05-25 20:31:18 +0000

    🛢️ Modelo de datos

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -7,19 +7,24 @@ import { ROOT_DIR } from '../helpers/paths.js';
 // Creando una instancia del enrutador de express
 const router = Router();
 
-// GET /add-product
+// Datos en memoria volatil
+export const products = [];
+
+// GET /admin/add-product
 router.get('/add-product', (req, res, next) => {
   // Servimos el formulario
   console.log("📢 Sirviendo formulario...");
-  console.log(`📢 ROOT_DIR: ${ROOT_DIR}`);
   res.sendFile(path.join(ROOT_DIR, 'views','add-product.html'));
 });
 
-// POST /add-product
+// POST /admin/add-product
 router.post('/add-product', (req, res) => {
-  // Realizaremos la extracción de
-  // parametros dentro de la peticion
-  console.log(req.body);
+  // Realizaremos la desestructuracion de
+  // "name" de la petición
+  const { title } = req.body;
+  // Agregamos el dato en la base de datos
+  products.push(title);
+  // Redireccionando
   res.redirect('/');
 });
 
