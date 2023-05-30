c26545370ba742b538bec46abb1b3cffc4628beb
rivalcoba <ivan.rivalcoba@gmail.com>
2023-05-30 17:02:43 +0000

    👯‍♀️ Entrega de listas

--- routes/shop.route.js
+++ routes/shop.route.js
@@ -1,20 +1,22 @@
 // Importando el enrutador de express
 import { Router } from 'express';
-// Importando el gestor de rutas
-import path from 'path';
+// Importando el arreglo de productos
+import { products } from './admin.route.js';
 
 // Creando una instancia del enrutador de express
 const router = Router();
-
-// Importando productos
-import { products } from './admin.route.js';
 
 // GET /
 router.get('/', (req, res)=>{
   // Mostrando productos en memoria
   console.log(products);
   console.log("📢 Sirviendo la ruta '/'");
-  res.render('shop', {shop: 'active', docTitle:"Shop"});
+  res.render('shop', { 
+    shop: 'active', 
+    docTitle:"Shop",
+    viewStyle: '/css/product.css',
+    products
+  });
 });
 
 // GET /about
